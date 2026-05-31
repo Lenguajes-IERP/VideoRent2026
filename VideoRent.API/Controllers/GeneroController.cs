ba75a1203a1839fba674b60de7ce6061f36189ca
Alvaro Mena Monge <alvamen@hotmail.com>
2026-05-31 13:07:20 -0600

    Creación del proyecto Web API

---
+++ VideoRent.API/Controllers/GeneroController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoRent.Business;
+using VideoRent.Domain;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VideoRent.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneroController : ControllerBase
+    {
+        private IConfiguration configuration;
+
+        public GeneroController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        // GET: api/<GeneroController>
+        [HttpGet]
+        public async Task<IEnumerable<Genero>> Get()
+        {
+            var connectionString = configuration["ConnectionStrings:VideoRentDB"];
+            GeneroBusiness generoBusiness = new GeneroBusiness(connectionString);
+            IEnumerable<Genero> generos = await generoBusiness.GetGeneros();
+            return generos;
+        }
+
+        // GET api/<GeneroController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GeneroController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GeneroController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GeneroController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
